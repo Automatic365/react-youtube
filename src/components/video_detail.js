b189aef64e3e9ed9fe6a67250ee20308765e70d1
Jason Hanna <jason.hanna01@gmail.com>
2016-11-15 10:20:28 -0700

    add lodash - change search frequency so user experience isn' so jarring

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const VideoDetail = ({video}) => {
+  if (!video){
+    return<div>Loading...</div>
+  }
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
 
@@ -12,7 +15,7 @@ const VideoDetail = ({video}) => {
       <div className="details">
         <div>{video.snippet.title}</div>
         <div>{video.snippet.description}</div>
-      <div>
+      </div>
     </div>
   )
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyCyCfZpyo5PBjB5w0-fVXLfN5vWrrtGyYE';
 
@@ -11,17 +13,34 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('Knicks')
+  };
 
-    YTSearch({key: API_KEY, term: 'Knicks'}, (videos) => {
-      this.setState({ videos });
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
-  }
+  };
+
   render(){
+
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-      <SearchBar />
-      <VideoList videos={this.state.videos} />
+      <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+      onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+      videos={this.state.videos} />
       </div>
     )
   }
